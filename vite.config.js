9f486013e3756ab0876c072880c955f4f5e6fae6
Guido Storino <guido.storino@gmail.com>
2026-03-05 10:03:03 -0300

    gale-ai en galealg

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/gale-ai/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index.js',   // nombre fijo, sin hash
+        chunkFileNames: 'assets/index.js',
+        assetFileNames: 'assets/index.[ext]', // index.css siempre
+      }
+    }
+  }
 })
